6e57f5dcd901ad06cb81e3db50b4eea514660972
D. Hristov <dean.hristov89@gmail.com>
2019-09-01 17:56:33 +0300

    Part of rev: 3640408

--- app/config/initialAppState.js
+++ app/config/initialAppState.js
@@ -1,5 +1,7 @@
+import AsyncStorage from '@react-native-community/async-storage';
 import i18nLoader from '@modules/i18nLoader';
 
+export const regExEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 export const credentials = {
     isLogged: false,
     isRemember: false,
@@ -16,7 +18,11 @@ export const asyncStore = {
 export const i18nState = {
     locale: 'bg',
     languages: ['en', 'bg'],
-    messages: i18nLoader('en') // TODO Extend it!
+    messages: i18nLoader('en')
+    // messages: async () => {
+    //     const locale = await AsyncStorage.getItem(asyncStore.locale);
+    //     return i18nLoader(locale || 'en')
+    // }
 };
 
 export const depositState = [];
